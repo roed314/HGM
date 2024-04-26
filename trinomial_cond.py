16ed2d580d0cc9a0d58160e4c89ca1e395ca7ca1
David Roe <roed.math@gmail.com>
2024-04-26 16:19:12 -0400

    Trying to wrap things in quotes

--- trinomial_cond.py
+++ trinomial_cond.py
@@ -123,7 +123,8 @@ else:
                         for k in range(*get_ks(p, w, r)):
                             _ = F.write(f"{w} {r} {p} {m} {k}\n")
     try:
-        subprocess.run(f"parallel -j {args.jobs} -a {jobfile} ./trinomial_cond.py --nograph{noramp} -w {{1}} -r {{2}} -p {{3}} -m {{4}} -k {{5}}", shell=True, check=True)
+        subprocess.run(f"parallel -j {args.jobs} -a {jobfile} ./trinomial_cond.py --nograph{noramp} -w=
+{{1}}' -r='{{2}}' -p='{{3}}' -m='{{4}}' -k='{{5}}'", shell=True, check=True)
     finally:
         jobfile.unlink()
 
